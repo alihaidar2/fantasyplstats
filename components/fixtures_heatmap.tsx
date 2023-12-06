074e51b146bc2a43c7bb89e37e1caa6487c96b20
ahaidar <ahaidar@oproma.com>
2023-12-05 18:43:43 -0800

    got heamtpa owrking with proper values

--- components/fixtures_heatmap.tsx
+++ components/fixtures_heatmap.tsx
@@ -4,13 +4,20 @@ import { Fixture } from '../types/Fixture';
 import { Team } from '../types/Team';
 
 
+
 const FixturesHeatmap: React.FC = () => {
 
     const [teams, setTeams] = useState<Team[]>([]); // passed to heatmap
     const [fixtures, setFixtures] = useState<Fixture[]>([]); // passed to heatmap
     const [heatmapData, setHeatmapData] = useState<any[][]>([]); // passed to heatmap
     const [gameweeks, setGameweeks] = useState<number[]>([]); // Initialize as an empty array
-    const [selectedGameweekRange, setSelectedGameweeks] = useState(5); // Default value
+    const [selectedGameweekRange, setSelectedGameweeks] = useState(gameweeks.length); // Default value
+    const [teamFixtureDictionary, setTeamFixtureDictionary] = useState<{ [teamName: string]: SimpleFixture[] }>({});
+
+    interface SimpleFixture {
+        opponentName: string;
+        difficulty: number;
+    }
 
 
     useEffect(() => {
@@ -22,17 +29,22 @@ const FixturesHeatmap: React.FC = () => {
                 setTeams(data.teams);
                 setFixtures(data.fixtures);
 
-                // TO-DO: Modify this to get actual heatmap data
-                const sampleHeatmapData = Array.from({ length: 20 }, () => Array.from({ length: 25 }, () => Math.floor(Math.random() * 5) + 1));
-                const customHeatmapData = sampleHeatmapData.map(row => row.slice(0, selectedGameweekRange));
-                console.log("heatmap data:", customHeatmapData)
-                setHeatmapData(customHeatmapData);
+                // Gets heatmap data as 2D array
+                const heatmapData = getHeatmapData();
+                setHeatmapData(heatmapData);
+
+                // Creates dictionary with each team's fixtures
+                const teamFixtureDictionary = teams.reduce((acc, team, index) => {
+                    // Use the team's short name as the key, and the corresponding heatmapData2 row as the value
+                    acc[team.short_name] = heatmapData[index];
+                    return acc;
+                }, {});
+
+                setTeamFixtureDictionary(teamFixtureDictionary)
 
                 // Get remaining Gameweeks
                 const uniqueGameweeks: number[] = Array.from(new Set(fixtures.map(fixture => fixture.event))); // all remaining gws
-                // const limitedGameweeks = uniqueGameweeks.slice(0, selectedGameweekRange);
                 setGameweeks(uniqueGameweeks);
-                // setSelectedGameweeks(limitedGameweeks.length);
 
             })
             .catch(error => console.error('Error:', error));
@@ -48,31 +60,84 @@ const FixturesHeatmap: React.FC = () => {
     };
 
 
-    // To be implemented
+
+
+    // On GW Range change
+    const handleGameweekRangeChange = (event) => {
+        setSelectedGameweeks(Number(event.target.value)); // this sets it but it doesnt reload with the proper data
+    };
+
     function getHeatmapData() {
+        // Initialize an object to hold all teams with their opponents and difficulties
+        const teamsOpponentsAndDifficulties: { [teamName: string]: SimpleFixture[] } = {};
+        // console.log("teamsOpponentsAndDifficulties before: ", teamsOpponentsAndDifficulties)
+
+        // Populate the object with team names as keys
+        teams.forEach(team => {
+            teamsOpponentsAndDifficulties[team.short_name] = [];
+        });
+
+        // Go through each fixture and add the opponent team and their difficulty
+        fixtures.forEach(fixture => {
+            // get home and away team
+            const homeTeam = teams.find(t => t.team_id === fixture.team_h);
+            const awayTeam = teams.find(t => t.team_id === fixture.team_a);
+
+            // add the opponent name and difficulty at the HOME team key
+            if (homeTeam) {
+                teamsOpponentsAndDifficulties[homeTeam.short_name].push({
+                    opponentName: awayTeam!.short_name,
+                    difficulty: fixture.team_h_difficulty,
+                });
+            }
+
+            // add the opponent name and difficulty at the AWAY team key
+            if (awayTeam) {
+                teamsOpponentsAndDifficulties[awayTeam.short_name].push({
+                    opponentName: homeTeam!.short_name,
+                    difficulty: fixture.team_a_difficulty
+                });
+            }
+        });
+        // finish with team's : (fixtures && difficulties)
+        // console.log("teamsOpponentsAndDifficulties after: ", teamsOpponentsAndDifficulties)
+
+        // Creates 2D array of fixtures, need to create a dictionary to map keys to this from teams
+        return Object.values(teamsOpponentsAndDifficulties).map(
+            (teamFixtures: SimpleFixture[]) => teamFixtures
+        );
 
     }
 
-    // On GW range change
-    const handleGameweekRangeChange = (event) => {
-        console.log("value chosen: ", event.target.value)
-        setSelectedGameweeks(Number(event.target.value)); // this sets it but it doesnt reload with the proper data
-        console.log("selectedGameweekRange: ", selectedGameweekRange)
-    };
 
-    const cellStyle = (background: any, value: number, min: number, max: number, data: any, x: any, y: any) => {
-        // Normalize the value
-        const normalizedValue = (value - min) / (max - min);
 
-        // Interpolate between green (lower difficulty) and red (higher difficulty)
-        const red = normalizedValue * 255; // More difficult = more red
-        const green = (1 - normalizedValue) * 255; // Less difficult = more green
+    // const teamFixtureDictionary = {};
 
-        return {
-            background: `rgb(${red}, ${green}, 0)`,
-            // ... other styles
-        };
-    };
+    // teams.forEach((team, index) => {
+    //     // Ensure that heatmapData has the same order and length as teams
+    //     teamFixtureDictionary[team.short_name] = heatmapData[index];
+    // });
+
+    console.log("teamFixtureDictionary: ", teamFixtureDictionary)
+
+
+    const getDifficultyColor = (difficulty: number): string => {
+        switch (difficulty) {
+            case 2:
+                return 'darkgreen'; // Difficulty 2
+            case 3:
+                return 'green';     // Difficulty 3
+            case 4:
+                return 'red';       // Difficulty 4
+            case 5:
+                return 'darkred';   // Difficulty 5
+            default:
+                return 'gray';      // Default color
+        }
+    }
+
+
+    // console.log("heatmapData: ", heatmapData)
 
     return (
         <div>
@@ -85,11 +150,38 @@ const FixturesHeatmap: React.FC = () => {
             <HeatMap
                 xLabels={gameweeks.slice(0, selectedGameweekRange)}
                 yLabels={teams.map(team => team.short_name)}
-                data={heatmapData}
-                // cellStyle={cellStyle}
-            // ... other props
-            />
+                data={heatmapData.map(row => row.map(cell => cell.difficulty))} // Heatmap expects a 2D array
+                cellStyle={(background, value, min, max, data, x, y) => {
+                    // Use your getDifficultyColor function to determine the background color
+                    const backgroundColor = getDifficultyColor(value);
+                    return {
+                        background: backgroundColor, // Set the background color based on the difficulty
+                        fontSize: '11px', // Set the font size or any other styles you need
+                        color: 'black', // Set the text color to black
+                        // Add any additional styles you want to apply to each cell
+                    };
+                }}
+                cellRender={(x, y, team) => {
+                    // Safeguard against undefined data
+                    if (!heatmapData[y] || heatmapData[y].length <= x) {
+                        return <div className="text-center text-black">N/A</div>; // Placeholder with Tailwind classes
+                    }
 
+                    // const fixture = heatmapData[y][x];
+                    const fixture = teamFixtureDictionary[team][y - gameweeks[0]]
+                    const backgroundColorClass = getDifficultyColor(fixture.difficulty); // This should return a Tailwind color class
+
+                    // Render the cell with the opponent name and the background color based on difficulty
+                    return (
+                        <div className={`text-center ${backgroundColorClass} text-black`}>
+                            {fixture.opponentName}
+                        </div>
+                    );
+                }}
+
+
+
+            />
         </div>
 
     );
