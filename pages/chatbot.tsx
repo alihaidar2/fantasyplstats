566c4a609fdef853d3789f5012d1357853014c41
ahaidar <ahaidar@oproma.com>
2024-04-10 14:20:21 -0700

    fixed

--- pages/chatbot.tsx
+++ pages/chatbot.tsx
@@ -1,13 +1,13 @@
 // pages/chat.tsx
 
 import React from "react";
-import ChatInterface from "../components/chat_interface";
+// import ChatInterface from "../components/chat_interface";
 
 const ChatPage = () => {
   return (
     <div>
       <h1>Chat with Our AI</h1>
-      <ChatInterface />
+      {/* <ChatInterface /> */}
     </div>
   );
 };
