38f619752ee573f73553f578e2b5ddc7da3d4431
ahaidar <ahaidar@oproma.com>
2024-12-19 23:43:47 -0500

    i needed it + cleanup

--- lib/cosmosClient.ts
+++ lib/cosmosClient.ts
@@ -9,23 +9,6 @@ const endpoint = process.env.COSMOS_DB_ENDPOINT!;
 const key = process.env.COSMOS_DB_KEY!;
 const databaseId = process.env.COSMOS_DB_DATABASE!;
 
-console.log("process.env: ", process.env);
-console.log("endpoint: ", endpoint);
-console.log("key: ", key);
-console.log("databaseId: ", databaseId);
-
-// if (!endpoint) {
-//   throw new Error("COSMOS_DB_ENDPOINT environment variable is missing");
-// }
-
-// if (!key) {
-//   throw new Error("COSMOS_DB_KEY environment variable is missing");
-// }
-
-// if (!databaseId) {
-//   throw new Error("COSMOS_DB_DATABASE environment variable is missing");
-// }
-
 // Initialize Cosmos DB client
 const client = new CosmosClient({ endpoint, key });
 
