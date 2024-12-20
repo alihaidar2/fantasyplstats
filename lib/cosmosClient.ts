7e5166e03a45efa4c998b9f9c51aa956c71c8139
ahaidar <ahaidar@oproma.com>
2024-12-19 23:05:31 -0500

    test

--- lib/cosmosClient.ts
+++ lib/cosmosClient.ts
@@ -13,22 +13,26 @@ console.log("endpoint: ", endpoint);
 console.log("key: ", key);
 console.log("databaseId: ", databaseId);
 
-if (!endpoint) {
-  throw new Error("COSMOS_DB_ENDPOINT environment variable is missing");
-}
+// if (!endpoint) {
+//   throw new Error("COSMOS_DB_ENDPOINT environment variable is missing");
+// }
 
-if (!key) {
-  throw new Error("COSMOS_DB_KEY environment variable is missing");
-}
+// if (!key) {
+//   throw new Error("COSMOS_DB_KEY environment variable is missing");
+// }
 
-if (!databaseId) {
-  throw new Error("COSMOS_DB_DATABASE environment variable is missing");
-}
+// if (!databaseId) {
+//   throw new Error("COSMOS_DB_DATABASE environment variable is missing");
+// }
 
 // Initialize Cosmos DB client
 const client = new CosmosClient({ endpoint, key });
 
+console.log("client: ", client);
+
 // Reference the Cosmos DB database
 const database = client.database(databaseId);
+
+console.log("database: ", database);
 
 export default database;
