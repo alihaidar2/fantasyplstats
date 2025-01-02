bc2442cce41390363bcfa742c42592f9d066ba35
ahaidar <ahaidar@oproma.com>
2025-01-02 17:23:00 -0500

    debug

--- lib/cosmosClient.ts
+++ lib/cosmosClient.ts
@@ -1,9 +1,7 @@
 import { CosmosClient } from "@azure/cosmos";
-// import dotenv from "dotenv";
 
-// Load environment variables from .env.local file
-// dotenv.config();
-
+console.log("process.env: ", process.env);
+console.log("process: ", process);
 console.log("in cosmos client");
 
 // Use environment variables to connect to Cosmos DB
