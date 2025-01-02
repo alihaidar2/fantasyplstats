c6db4f13ba8e6c08acd755362336320578ec979a
ahaidar <ahaidar@oproma.com>
2025-01-02 00:05:08 -0500

    added loggin to cosmos

--- lib/cosmosClient.ts
+++ lib/cosmosClient.ts
@@ -4,9 +4,20 @@ import { CosmosClient } from "@azure/cosmos";
 // Load environment variables from .env.local file
 // dotenv.config();
 
+console.log("in cosmos client");
+
 // Use environment variables to connect to Cosmos DB
 const endpoint = process.env.COSMOS_DB_ENDPOINT!;
 const key = process.env.COSMOS_DB_KEY!;
+if (!endpoint || !key) {
+  console.error("Missing Cosmos DB endpoint or key environment variables");
+}
+console.log("COSMOS_DB_ENDPOINT:", process.env.COSMOS_DB_ENDPOINT);
+console.log(
+  "COSMOS_DB_KEY:",
+  process.env.COSMOS_DB_KEY ? "Key is present" : "Key is missing"
+);
+
 const databaseId = process.env.COSMOS_DB_DATABASE!;
 
 // Initialize Cosmos DB client
