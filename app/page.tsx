19d2e03117047871849e5c66788adf6cef77fa3e
ahaidar <ahaidar@oproma.com>
2025-01-03 14:50:28 -0500

    added SSR to home page

--- app/page.tsx
+++ app/page.tsx
@@ -42,3 +42,10 @@ export default function Home() {
     </div>
   );
 }
+
+// Server-side rendering
+export async function getServerSideProps() {
+  return {
+    props: {}, // No dynamic data, just ensure SSR is enabled
+  };
+}
