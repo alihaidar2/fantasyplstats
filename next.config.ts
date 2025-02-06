ad925f91bdd3a2f2b0f3cdca7c4c909b1e97bf09
ahaidar <ahaidar@oproma.com>
2025-02-05 21:20:36 -0500

    test again

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {},
+  },
+};
 
 export default nextConfig;
