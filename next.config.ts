660b175de918992e1af9ce96d076687bdd61371b
ahaidar <ahaidar@oproma.com>
2025-06-29 14:42:17 -0400

    build error potential fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
   async headers() {
     return [
       {
